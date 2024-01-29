c4da30ee828bcd250ffc61f0077e28c15e99a6fe
jaeseung.bae <jaeseung.bae@linecorp.com>
2024-01-29 13:51:36 +0900

    chore: test

--- x/bankplus/module.go
+++ x/bankplus/module.go
@@ -1,30 +1,12 @@
 package bankplus
 
 import (
-	"github.com/cosmos/cosmos-sdk/codec"
-	"github.com/cosmos/cosmos-sdk/types/module"
-	accountkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
 	"github.com/cosmos/cosmos-sdk/x/bank"
 	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
-	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
-)
-
-var (
-	_ module.AppModule           = AppModule{}
-	_ module.AppModuleSimulation = AppModule{}
 )
 
 type AppModule struct {
 	bank.AppModule
 
 	bankKeeper bankkeeper.Keeper
-}
-
-func NewAppModule(cdc codec.Codec, keeper bankkeeper.Keeper, accountKeeper accountkeeper.AccountKeeper) AppModule {
-	return AppModule{}
-}
-
-func (am AppModule) RegisterServices(cfg module.Configurator) {
-	banktypes.RegisterMsgServer(cfg.MsgServer(), bankkeeper.NewMsgServerImpl(am.bankKeeper))
-	banktypes.RegisterQueryServer(cfg.QueryServer(), am.bankKeeper)
 }
