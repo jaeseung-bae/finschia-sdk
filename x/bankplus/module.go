ba4e8430bea0452dfd46de11a9aada18a715333a
jaeseung.bae <jaeseung.bae@linecorp.com>
2024-01-29 13:47:02 +0900

    chore: copy bankplus

--- x/bankplus/module.go
+++ x/bankplus/module.go
@@ -5,7 +5,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/module"
 	accountkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
 	"github.com/cosmos/cosmos-sdk/x/bank"
-	"github.com/cosmos/cosmos-sdk/x/bank/exported"
 	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 )
@@ -18,20 +17,15 @@ var (
 type AppModule struct {
 	bank.AppModule
 
-	bankKeeper     bankkeeper.Keeper
-	legacySubspace exported.Subspace
+	bankKeeper bankkeeper.Keeper
+	//legacySubspace exported.Subspace
 }
 
-func NewAppModule(cdc codec.Codec, keeper bankkeeper.Keeper, accountKeeper accountkeeper.AccountKeeper, ss exported.Subspace) AppModule {
-	return AppModule{
-		AppModule:      bank.NewAppModule(cdc, keeper, accountKeeper, ss),
-		bankKeeper:     keeper,
-		legacySubspace: ss,
-	}
+func NewAppModule(cdc codec.Codec, keeper bankkeeper.Keeper, accountKeeper accountkeeper.AccountKeeper) AppModule {
+	return AppModule{}
 }
 
 func (am AppModule) RegisterServices(cfg module.Configurator) {
 	banktypes.RegisterMsgServer(cfg.MsgServer(), bankkeeper.NewMsgServerImpl(am.bankKeeper))
 	banktypes.RegisterQueryServer(cfg.QueryServer(), am.bankKeeper)
-
 }
