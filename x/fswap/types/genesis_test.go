9c78ab776e08c36cd74b2c4671fe8c7ea6795809
jaeseung.bae <jaeseung.bae@linecorp.com>
2024-05-08 15:20:52 +0900

    chore: add test for genesis validation

--- x/fswap/types/genesis_test.go
+++ x/fswap/types/genesis_test.go
@@ -3,31 +3,115 @@ package types_test
 import (
 	"testing"
 
+	sdk "github.com/Finschia/finschia-sdk/types"
 	"github.com/stretchr/testify/require"
 
 	"github.com/Finschia/finschia-sdk/x/fswap/types"
 )
 
-// todo: add tests
-func TestGenesisStateValidate(t *testing.T) {
+func TestValidateGenesis(t *testing.T) {
 	for _, tc := range []struct {
-		desc     string
+		name     string
 		genState *types.GenesisState
-		valid    bool
+		wantErr  bool
 	}{
 		{
-			desc:     "default is valid",
+			name:     "valid default genesis",
 			genState: types.DefaultGenesis(),
-			valid:    true,
+			wantErr:  false,
+		},
+		{
+			name: "valid: initial empty genesis",
+			genState: &types.GenesisState{
+				Swaps: []types.Swap{},
+				SwapStats: types.SwapStats{
+					SwapCount: 0,
+				},
+				Swappeds: []types.Swapped{},
+			},
+			wantErr: false,
+		},
+		{
+			name: "invalid: the number of Swaps should match to the number of Swappeds",
+			genState: &types.GenesisState{
+				Swaps: []types.Swap{
+					{
+						FromDenom:           "from",
+						ToDenom:             "toD",
+						AmountCapForToDenom: sdk.OneInt(),
+						SwapRate:            sdk.NewDec(12),
+					},
+				},
+				SwapStats: types.SwapStats{},
+				Swappeds:  nil,
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid: SwapCount should match to the number of Swaps",
+			genState: &types.GenesisState{
+				Swaps: []types.Swap{},
+				SwapStats: types.SwapStats{
+					SwapCount: 1,
+				},
+				Swappeds: []types.Swapped{},
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid: invalid Swap element with zero AmountCapForToDenom",
+			genState: &types.GenesisState{
+				Swaps: []types.Swap{
+					{
+						FromDenom:           "from",
+						ToDenom:             "toD",
+						AmountCapForToDenom: sdk.ZeroInt(),
+						SwapRate:            sdk.NewDec(12),
+					},
+				},
+				SwapStats: types.SwapStats{
+					SwapCount: 1,
+				},
+				Swappeds: []types.Swapped{
+					{
+						FromCoinAmount: sdk.NewCoin("from", sdk.ZeroInt()),
+						ToCoinAmount:   sdk.NewCoin("toD", sdk.ZeroInt()),
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid: invalid Swap element with zero SwapRate",
+			genState: &types.GenesisState{
+				Swaps: []types.Swap{
+					{
+						FromDenom:           "from",
+						ToDenom:             "toD",
+						AmountCapForToDenom: sdk.OneInt(),
+						SwapRate:            sdk.NewDec(0),
+					},
+				},
+				SwapStats: types.SwapStats{
+					SwapCount: 1,
+				},
+				Swappeds: []types.Swapped{
+					{
+						FromCoinAmount: sdk.NewCoin("from", sdk.ZeroInt()),
+						ToCoinAmount:   sdk.NewCoin("toD", sdk.ZeroInt()),
+					},
+				},
+			},
+			wantErr: true,
 		},
 	} {
-		t.Run(tc.desc, func(t *testing.T) {
+		t.Run(tc.name, func(t *testing.T) {
 			err := tc.genState.Validate()
-			if tc.valid {
-				require.NoError(t, err)
-			} else {
+			if tc.wantErr {
 				require.Error(t, err)
+				return
 			}
+			require.NoError(t, err)
 		})
 	}
 }
